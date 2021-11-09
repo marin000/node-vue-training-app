0637b6b74244ae9fee671c446344004ea11d02e9
[marin buric] <[marin.buric.agilathon.com]>
2021-11-09 11:18:41 +0100

    db connect to index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@
  * Required External Modules
  */
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config()
 
 /**
  * App Variables
@@ -13,6 +15,22 @@ const port = process.env.PORT || "4101";
 const router = require('./router');
 app.use(express.json());
 app.use(router);
+
+/**
+ * DB connection
+ */
+const url = process.env.DB_URL;
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+mongoose.connect(url, connectionParams)
+  .then(() => {
+    console.log('Connected to database ')
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  })
 
 /**
  * Server Activation
