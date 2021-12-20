a8a029cb5d52dbd7879e1763a44541ad2f6bf693
[marin buric] <[marin.buric.agilathon.com]>
2021-12-20 10:23:19 +0100

    manual seeding fixed

--- server/dbSeeding/manual.js
+++ server/dbSeeding/manual.js
@@ -1,4 +1,22 @@
 const seedingService = require('../service/seeding');
+const mongoose = require('mongoose');
+const { dbConnectionLogger, simpleLogger } = require('../logger/logger');
+const email = require('../service/email');
+const config = require('../config/index');
 
-seedingService.employees();
-seedingService.tasks();
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+mongoose.connect(config.dbUrl, connectionParams)
+  .then(() => {
+    seedingService.seedEmployees();
+    seedingService.seedTasks();
+    simpleLogger.info('Connect to database');
+    dbConnectionLogger.info('Conneted to database');
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+    dbConnectionLogger.error(`Error connecting to the database. \n${err}`);
+    email.sendEmail(`Error connecting to the database. \n${err}`);
+  })
