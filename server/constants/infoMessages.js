cf2b468c1f4344febe22826a7a9132022c909cf8
[marin buric] <[marin.buric.agilathon.com]>
2021-12-23 13:08:25 +0100

    info msg constants

--- server/constants/infoMessages.js
+++ server/constants/infoMessages.js
@@ -1,7 +1,9 @@
 module.exports = {
   NEW_EMPLOYEE: 'New employee created.',
   GET_EMPLOYEES: 'Successfully get all employees.',
+  DELETE_EMPLOYEE: 'Employee deleted successfully.',
   NEW_TASK: 'New task created.',
+  DELETE_TASK: 'Task deleted successfully.',
   UPDATE_TASK: 'Task updated successfully.',
   GET_TASKS: 'Get employee tasks successfully.',
   GET_LOGS: 'Successfully get all logs.',
