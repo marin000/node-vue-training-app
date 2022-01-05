80570079a80c360697c1d6617f09e300f32c0ead
[marin buric] <[marin.buric.agilathon.com]>
2022-01-05 15:25:58 +0100

    create pdf with promisify

--- server/service/report.js
+++ server/service/report.js
@@ -4,18 +4,7 @@ const wkhtmltopdf = require('wkhtmltopdf');
 const { simpleLogger } = require('../logger/logger');
 const Report = require('../Models/Reports');
 const report = require('../constants/report');
-
-function createPdf(html, filePath) {
-  return new Promise((resolve, reject) => {
-    wkhtmltopdf(html, { output: filePath, pageSize: 'a4' }, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    })
-  });
-}
+const Promise = require('bluebird');
 
 async function generateReport({ data, template, reportDir, pdfName }) {
   if (!fs.existsSync(reportDir)) {
@@ -23,7 +12,8 @@ async function generateReport({ data, template, reportDir, pdfName }) {
   }
   const filePath = `${reportDir}/${pdfName}`;
   const html = await ejs.renderFile(template, data, { async: true });
-  await createPdf(html, filePath);
+  const createPdf = Promise.promisify(wkhtmltopdf);
+  await createPdf(html, { output: filePath, pageSize: 'a4' });
   const reportPath = `${report.REPORTS_PATH}/${data.employee._id}/${pdfName}`;
   const newReport = Report({ path: reportPath });
   await newReport.save();
