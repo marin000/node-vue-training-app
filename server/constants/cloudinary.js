20f2060629c51cbed4a670746a892122c2856137
[marin buric] <[marin.buric.agilathon.com]>
2022-01-11 15:02:55 +0100

    double to single quotes

--- server/constants/cloudinary.js
+++ server/constants/cloudinary.js
@@ -1,5 +1,6 @@
 module.exports = {
-  uploadImgOptions: { width: 1920, height: 1080, crop: "crop" },
+  uploadImgOptions: { width: 1920, height: 1080, crop: 'crop' },
   imgOptions: {
-    height: 100, radius: "max", width: 100, crop: "thumb", quality: "auto"}
+    height: 100, radius: 'max', width: 100, crop: 'thumb', quality: 'auto'
+  }
 }
